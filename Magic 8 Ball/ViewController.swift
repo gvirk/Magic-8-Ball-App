dd574d6b726d8643f8082d91e9ced8e442b57a6e
Gursharnbir Singh <gursharnbirsingh@gursharnbirs-macbook-pro.local>
2018-07-03 12:56:14 -0600

    Added Magic 8 ball images and shake gestures

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let ballImageArray = ["ball1", "ball2", "ball3", "ball4", "ball5", "ball6"]
+    
+    var randomBallNumber : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        randomImageGen()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonPress(_ sender: UIButton) {
+        randomImageGen()
+    }
+    
+    func randomImageGen(){
+        randomBallNumber = Int(arc4random_uniform(5))
+        imageView.image = UIImage(named: ballImageArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        randomImageGen()
+    }
 }
 
